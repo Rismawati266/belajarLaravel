a61895012975c0c434f07a5fdd14ca3245ceab5d
Rismawati266 <risma.wati266@mail.unpas.ac.id>
2019-12-04 16:35:36 +0700

    menampilkan data nama mahasiswa yang akan diubah

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -13,21 +13,21 @@
             @csrf
             <div class="form-group">
                 <label for="nama">Nama</label>
-                <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Masukkan Nama" name="nama" value="{{ old( 'nama' ) }}">
+                <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Masukkan Nama" name="nama" value="{{ $student->nama }}">
                 @error('nama') <div class="invalid-feedback">{{ $message }}</div>@enderror
             </div>
             <div class="form-group">
                 <label for="nrp">NRP</label>
-                <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" placeholder="Masukkan NRP" name="nrp" value="{{ old( 'nrp' ) }}">
+                <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" placeholder="Masukkan NRP" name="nrp" value="{{ $student->nrp }}">
                 @error('nrp') <div class="invalid-feedback">{{ $message }}</div>@enderror
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="text" class="form-control" id="email" placeholder="Masukkan Email" name="email" value="{{ old( 'email' ) }}">
+                <input type="text" class="form-control" id="email" placeholder="Masukkan Email" name="email" value="{{ $student->email }}">
             </div>
             <div class="form-group">
                 <label for="jurusan">Jurusan</label>
-                <input type="text" class="form-control" id="jurusan" placeholder="Masukkan Jurusan" name="jurusan" value="{{ old( 'jurusan' ) }}">
+                <input type="text" class="form-control" id="jurusan" placeholder="Masukkan Jurusan" name="jurusan" value="{{ $student->jurusan }}">
             </div>
             <button type="submit" class="btn btn-primary">Ubah Data!</button>
         </form>
