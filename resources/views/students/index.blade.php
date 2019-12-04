bdea718a1a983438ab48f22d9db9b6c0e57b2a6e
Rismawati266 <risma.wati266@mail.unpas.ac.id>
2019-12-04 12:37:17 +0700

    menambahkan tombol tambah data di file index.blade.php

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -9,6 +9,8 @@
       <div class="col-6">
         <h1 class="mt-3">Daftar Mahasiswa</h1>
 
+        <a href="/students/create" class="btn btn-primary my-3">Tmbah Data Mahasiswa</a>
+
             <ul class="list-group">
                 @foreach( $students as $student )
                     <li class="list-group-item d-flex justify-content-between align-items-center">
